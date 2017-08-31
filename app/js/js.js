c4819d379f889991161eb57c39cf5642d091cdf2
enmalafeev <malafeev.en@yandex.ru>
2017-08-31 23:36:00 +0300

    js

--- app/js/js.js
+++ app/js/js.js
@@ -1,55 +1,55 @@
 ymaps.ready(init);
-    var myMap,
-    	myPlacemark1,
-    	myPlacemark;
+var myMap,
+    myPlacemark1,
+    myPlacemark;
 
 
-    function init(){     
-        myMap = new ymaps.Map("map", {
-            center: [59.67068375, 29.90353240],
-            zoom: 16
-        });
+function init() {
+    myMap = new ymaps.Map("map", {
+        center: [59.67068375, 29.90353240],
+        zoom: 16
+    });
 
-        myPlacemark1 = new ymaps.Placemark([59.67068375, 29.90353240], { 
-        	balloonContentHeader: 'Пилорама Silva',
-		    balloonContentBody: 'Пилорама Silva',
-		    balloonContentFooter: 'Брус, доска по лучшим ценам',
-		    hintContent: 'Пиломатериалы, напрямую от производителя'
-        });
+    myPlacemark1 = new ymaps.Placemark([59.67068375, 29.90353240], {
+        balloonContentHeader: 'Пилорама Silva',
+        balloonContentBody: 'Пилорама Silva',
+        balloonContentFooter: 'Брус, доска по лучшим ценам',
+        hintContent: 'Пиломатериалы, напрямую от производителя'
+    });
 
-        // myMap.geoObjects.add(myPlacemark1);
+    // myMap.geoObjects.add(myPlacemark1);
 
-        var myPlacemark = new ymaps.Placemark([59.67068375, 29.90353240], {
-        	balloonContentHeader: 'Пилорама Silva',
-		    balloonContentBody: 'Пилорама Silva',
-		    balloonContentFooter: 'Брус, доска по лучшим ценам',
-		    hintContent: 'Пиломатериалы, напрямую от производителя'
-        }, {
-	        iconLayout: 'default#image',
-	        iconImageHref: 'img/png/pointer.png',
-	        iconImageSize: [60, 64],
-	        iconImageOffset: [-30, -74]
-	    });
+    var myPlacemark = new ymaps.Placemark([59.67068375, 29.90353240], {
+        balloonContentHeader: 'Пилорама Silva',
+        balloonContentBody: 'Пилорама Silva',
+        balloonContentFooter: 'Брус, доска по лучшим ценам',
+        hintContent: 'Пиломатериалы, напрямую от производителя'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/png/pointer.png',
+        iconImageSize: [60, 64],
+        iconImageOffset: [-30, -74]
+    });
 
-	    myMap.geoObjects.add(myPlacemark);
+    myMap.geoObjects.add(myPlacemark);
 
-    }
+}
 
 var btn = document.querySelectorAll(".btn");
 var popup = document.querySelector(".modal-content");
-var close = document.querySelector(".modal-content__close")
-var overlay = document.querySelector(".modal-overlay")
+var close = document.querySelector(".modal-content__close");
+var overlay = document.querySelector(".modal-overlay");
 
-for(var i=0; i<btn.length; i++){
-    btn[i].addEventListener('click', function(event){
+for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function(event) {
         event.preventDefault();
         popup.classList.add("modal-content-show");
-        overlay.classList.add("modal-overlay-show");  
+        overlay.classList.add("modal-overlay-show");
     });
 }
 
 
-close.addEventListener("click", function(event){
+close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("modal-content-show");
     overlay.classList.remove("modal-overlay-show");
@@ -72,10 +72,3 @@ close.addEventListener("click", function(event){
 //     popup.classList.remove("modal-content-show");
 //     overlay.classList.remove("modal-overlay-show");
 // });
-
-
-
-
-
-
-
